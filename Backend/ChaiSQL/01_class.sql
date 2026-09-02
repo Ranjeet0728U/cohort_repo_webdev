3673d324e949dd7e14d549191b33c91b5249bb3b
Ranjeet0728U <upadhyayr0728@gmail.com>
2026-09-03 00:50:41 +0530

    db learning

--- Backend/ChaiSQL/01_class.sql
+++ Backend/ChaiSQL/01_class.sql
@@ -1,1 +1,27 @@
-CREATE DATABASE student;
+-- -- SQLBook: Code
+-- CREATE TABLE students(
+--     student_id SERIAL PRIMARY KEY,
+
+--     first_name VARCHAR(50) NOT NULL,
+--     last_name VARCHAR(50),
+
+--     email VARCHAR(322) UNIQUE NOT NULL,
+--     phone_number INT,
+
+--     age INT CHECK (age > 12),
+
+--     current_status VARCHAR(20) DEFAULT 'active' CHECK(current_status IN ('active', 'graduated', 'dropper_out')),
+
+--     masterji_handle VARCHAR(50) UNIQUE,
+
+--     has_joined_masterji BOOLEAN DEFAULT FALSE,
+
+--     current_score INT DEFAULT 0 CHECK(current_score >= 0 AND current_score <= 100),
+--     enrollment_date DATE DEFAULT CURRENT_DATE
+
+-- );
+
+ALTER TABLE students
+ADD COLUMN batch_name VARCHAR(150) DEFAULT 'web dev 2026';
+
+
